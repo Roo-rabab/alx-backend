c6c155c63f49f6a984a6a40d3b976f76efa8f8f8
Roo-rabab <rababmchich312@gmail.com>
2024-03-12 16:15:04 -0700

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
+""" doc doc doc """
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    Configuration for Babel
-    """
+    """doc doc doc"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,13 +17,11 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('1-index.html')
+@app.route("/")
+def index():
+    """doc doc doc"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port="5000")
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+""" doc doc doc """
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    Configuration for Babel
-    """
+    """doc doc doc"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -26,19 +19,15 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """doc doc doc"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('2-index.html')
+@app.route("/")
+def index():
+    """doc doc doc"""
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port="5000")
